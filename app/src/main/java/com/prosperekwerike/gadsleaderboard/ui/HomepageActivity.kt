8604a2a1ce704ab8da3ec9d5159fb09c0c7c4863
Prosper Ekwerike <pekwerike@gmail.com>
2020-08-31 22:15:54 +0100

    Working

--- app/src/main/java/com/prosperekwerike/gadsleaderboard/ui/HomepageActivity.kt
+++ app/src/main/java/com/prosperekwerike/gadsleaderboard/ui/HomepageActivity.kt
@@ -3,27 +3,29 @@ package com.prosperekwerike.gadsleaderboard.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.prosperekwerike.gadsleaderboard.R
 import com.prosperekwerike.gadsleaderboard.databinding.ActivityHomepageBinding
+import com.prosperekwerike.gadsleaderboard.ui.adapters.HomepageViewPagerAdapter
 import com.prosperekwerike.gadsleaderboard.viewmodels.HomepageActivityViewModel
 
 class HomepageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomepageBinding
-    private lateinit var homepageToolbar : Toolbar
+    private lateinit var homepageTabLayout : TabLayout
     private lateinit var homepageViewPager : ViewPager
     private val homepageViewModel: HomepageActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_homepage)
         initializeViews()
-
+        homepageViewPager.adapter = HomepageViewPagerAdapter(2, supportFragmentManager)
+        homepageTabLayout.addTab()
     }
 
     private fun initializeViews() {
-        homepageToolbar = binding.mainToolbar
+        homepageTabLayout = binding.mainToolbar
         homepageViewPager = binding.mainViewpager
     }
 }
--- app/src/main/java/com/prosperekwerike/gadsleaderboard/ui/adapters/HomepageViewPagerAdapter.kt
+++ app/src/main/java/com/prosperekwerike/gadsleaderboard/ui/adapters/HomepageViewPagerAdapter.kt
@@ -1,2 +1,22 @@
 package com.prosperekwerike.gadsleaderboard.ui.adapters
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.prosperekwerike.gadsleaderboard.ui.fragments.LearningLeadersFragment
+import com.prosperekwerike.gadsleaderboard.ui.fragments.SkillIQLeaders
+
+class HomepageViewPagerAdapter(
+    private val numberOfPages : Int, fm: FragmentManager
+) : FragmentStatePagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+    return when(position){
+           0 -> LearningLeadersFragment()
+           1 -> SkillIQLeaders()
+           else -> LearningLeadersFragment()
+       }
+    }
+
+    override fun getCount(): Int = numberOfPages
+
+}

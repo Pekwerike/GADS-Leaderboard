6587d8f249a98928362333ae43659529825ab0c2
Prosper Ekwerike <pekwerike@gmail.com>
2020-08-31 16:15:15 +0100

    First thing first

---
+++ app/src/main/java/com/prosperekwerike/gadsleaderboard/models/LearningLeadersCustomModel.kt
@@ -0,0 +1,7 @@
+package com.prosperekwerike.gadsleaderboard.models
+
+data class LearningLeadersCustomModel(
+    val leaderName : String,
+    val hoursAndCountry : String,
+    val learningBadgeUrl : String
+)
---
+++ app/src/main/java/com/prosperekwerike/gadsleaderboard/models/SkillsIQLeadersModel.kt
@@ -0,0 +1,8 @@
+package com.prosperekwerike.gadsleaderboard.models
+
+data class SkillsIQLeadersModel(
+    val name : String,
+    val score : Int,
+    val country : String,
+    val badgeUrl : String
+)

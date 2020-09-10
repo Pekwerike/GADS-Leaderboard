e8f5daeadde251216d83b7180d341563c3df3dfd
Prosper Ekwerike <pekwerike@gmail.com>
2020-09-10 11:27:04 +0100

    Optimized blazier view drawing speed by refactoring the code

--- app/src/main/java/com/prosperekwerike/gadsleaderboard/customviews/Blaziers.kt
+++ app/src/main/java/com/prosperekwerike/gadsleaderboard/customviews/Blaziers.kt
@@ -12,11 +12,32 @@ class Blaziers @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-   private val paint = Paint().apply {
+    private var mHeight = 0
+    private var mWidth = 0
+
+    private val paint = Paint().apply {
         isDither = true
         isAntiAlias = true
         color = resources.getColor(R.color.darken_ray)
         style = Paint.Style.FILL
+    }
+
+   private var topPath : Path = Path()
+   private var bottomPath : Path = Path()
+
+    private fun initializePaths() {
+         topPath = Path().apply {
+            moveTo(0F, 0F)
+            lineTo(0F, mHeight.toFloat() * 0.25F)
+            quadTo(mWidth / 2f, mHeight / 4.5f, mWidth.toFloat() * 0.35F, 0F)
+            lineTo(0F, 0F)
+        }
+         bottomPath = Path().apply {
+            moveTo(0F, mHeight.toFloat())
+            lineTo(mWidth.toFloat(), mHeight.toFloat())
+            lineTo(mWidth.toFloat(), mHeight / 1.5f)
+            quadTo(mWidth / 3f, mHeight / 1.3f, 0F, mHeight.toFloat() * 0.95F)
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -25,26 +46,20 @@ class Blaziers @JvmOverloads constructor(
         drawTopCurvedPath(canvas)
     }
 
-    private fun drawTopCurvedPath(canvas: Canvas?){
-        val path = Path().apply {
-            moveTo(0F, 0F)
-            lineTo(0F, height.toFloat() * 0.25F)
-            quadTo(width/2f, height/4.5f, width.toFloat() * 0.35F, 0F)
-            lineTo(0F, 0F)
-        }
-
-        canvas!!.drawPath(path, paint)
+    private fun drawTopCurvedPath(canvas: Canvas?) {
+        canvas!!.drawPath(topPath, paint)
     }
 
-    private fun drawBottomCurvedPath(canvas : Canvas?){
-        val path = Path().apply {
-            moveTo(0F, height.toFloat())
-            lineTo(width.toFloat(), height.toFloat())
-            lineTo(width.toFloat(), height/1.5f)
-            quadTo(width/3f, height/1.3f, 0F, height.toFloat() * 0.95F)
-        }
+    private fun drawBottomCurvedPath(canvas: Canvas?) {
+        canvas!!.drawPath(bottomPath, paint)
+    }
 
-        canvas!!.drawPath(path, paint)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mWidth = MeasureSpec.getSize(widthMeasureSpec)
+        mHeight = MeasureSpec.getSize(heightMeasureSpec)
+
+        initializePaths()
     }
 
 
